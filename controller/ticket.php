cfb52c8a8e3005d86e069e0a72d096b4fe0d88f9
eduardopacompialopez <eduardopacompialopez@gmail.com>
2023-02-01 16:55:12 -0500

    compelto hasta el video 33 de udemy

--- controller/ticket.php
+++ controller/ticket.php
@@ -1,31 +1,31 @@
 <?php
-    
-    require_once("../config/conexion.php");
-    require_once("../models/Ticket.php");
-    $ticket = new Ticket();
 
-    switch ($_GET["op"]) {
+require_once("../config/conexion.php");
+require_once("../models/Ticket.php");
+$ticket = new Ticket();
+
+switch ($_GET["op"]) {
     case "insert":
-        $ticket->insert_ticket($_POST["usu_id"],$_POST["cat_id"],$_POST["tick_titulo"],$_POST["tick_descrip"]); //LLAMANDO A LOS DATOS DE Ticket.php para pasarlos como parametros
-        
+        $ticket->insert_ticket($_POST["usu_id"], $_POST["cat_id"], $_POST["tick_titulo"], $_POST["tick_descrip"]); //LLAMANDO A LOS DATOS DE Ticket.php para pasarlos como parametros
+
         break;
 
-    
+
     case "listar_x_usu":
         $datos = $ticket->listar_ticket_x_usu($_POST["usu_id"]);
         $data = array();
-        foreach($datos as $row){
-            $sub_array=array();
-            $sub_array[]=$row["tick_id"];
-            $sub_array[]=$row["cat_nom"];
-            $sub_array[]=$row["tick_titulo"];
-            
-            if ($row["tick_estado"]=="Abierto") {
+        foreach ($datos as $row) {
+            $sub_array = array();
+            $sub_array[] = $row["tick_id"];
+            $sub_array[] = $row["cat_nom"];
+            $sub_array[] = $row["tick_titulo"];
+
+            if ($row["tick_estado"] == "Abierto") {
                 $sub_array[] = '<span class="label label-pill label-success">Abierto</span>';
-                }else {   
-                    $sub_array[] = '<span class="label label-pill label-danger">Cerrado</span>';
-                }
-                
+            } else {
+                $sub_array[] = '<span class="label label-pill label-danger">Cerrado</span>';
+            }
+
             //$sub_array[]=$row["tick_estado"];  /*Para listar el estado del tiket en el datatable agrego este campo tick_estado que agrege en mi tabla mysql */
             //Aqui agrego el campo de fecha de creacion dandole el formato de dia mes año y hora minuto segundos 
             $sub_array[] = date("d/m/Y H:i:s", strtotime($row["fech_crea"])); //agrego este campo para mostrar en mi datatble, de estos parametros depende los campos que se mostraran en la datatabla es dcir si agrego uno mas se agregara automaticamente al datatable
@@ -33,47 +33,98 @@
             $data[] = $sub_array;
         }
         //Para usar el DATATABLE
-        $results= array(
-            "sEcho"=>1,
-            "iTotalRecords"=>count($data),
-            "iTotalDisplayRecords"=>count($data),
-            "aaData" => $data);
+        $results = array(
+            "sEcho" => 1,
+            "iTotalRecords" => count($data),
+            "iTotalDisplayRecords" => count($data),
+            "aaData" => $data
+        );
         echo json_encode($results);
         break;
-    
-        case "listar":
-            $datos = $ticket->listar_ticket();
-            $data = array();
-            foreach($datos as $row){
-                $sub_array=array();
-                $sub_array[]=$row["tick_id"];
-                $sub_array[]=$row["cat_nom"];
-                $sub_array[]=$row["tick_titulo"];
+
+    case "listar":
+        $datos = $ticket->listar_ticket();
+        $data = array();
+        foreach ($datos as $row) {
+            $sub_array = array();
+            $sub_array[] = $row["tick_id"];
+            $sub_array[] = $row["cat_nom"];
+            $sub_array[] = $row["tick_titulo"];
 
 
-                if ($row["tick_estado"]=="Abierto") {
+            if ($row["tick_estado"] == "Abierto") {
                 $sub_array[] = '<span class="label label-pill label-success">Abierto</span>';
-                }else {   
-                    $sub_array[] = '<span class="label label-pill label-danger">Cerrado</span>';
-                }
-                
-
-                //$sub_array[]=$row["tick_estado"];  /*Para listar el estado del tiket en el datatable agrego este campo tick_estado que agrege en mi tabla mysql */
-                //Aqui agrego el campo de fecha de creacion dandole el formato de dia mes año y hora minuto segundos 
-                $sub_array[] = date("d/m/Y H:i:s", strtotime($row["fech_crea"])); //agrego este campo para mostrar en mi datatble, de estos parametros depende los campos que se mostraran en la datatabla es dcir si agrego uno mas se agregara automaticamente al datatable
-                $sub_array[] = '<button type="button" onClick="ver(' . $row["tick_id"] . ');" id="' . $row["tick_id"] . '" class="btn btn-inline btn-primary btn-sm ladda-buttom"><div><i class="fa fa-eye"></i></div></button>'; //CREO ESTE BOTON 
-                $data[] = $sub_array;
+            } else {
+                $sub_array[] = '<span class="label label-pill label-danger">Cerrado</span>';
             }
-            //Para usar el DATATABLE
-            $results= array(
-                "sEcho"=>1,
-                "iTotalRecords"=>count($data),
-                "iTotalDisplayRecords"=>count($data),
-                "aaData" => $data);
-            echo json_encode($results);
+
+
+            //$sub_array[]=$row["tick_estado"];  /*Para listar el estado del tiket en el datatable agrego este campo tick_estado que agrege en mi tabla mysql */
+            //Aqui agrego el campo de fecha de creacion dandole el formato de dia mes año y hora minuto segundos 
+            $sub_array[] = date("d/m/Y H:i:s", strtotime($row["fech_crea"])); //agrego este campo para mostrar en mi datatble, de estos parametros depende los campos que se mostraran en la datatabla es dcir si agrego uno mas se agregara automaticamente al datatable
+            $sub_array[] = '<button type="button" onClick="ver(' . $row["tick_id"] . ');" id="' . $row["tick_id"] . '" class="btn btn-inline btn-primary btn-sm ladda-buttom"><div><i class="fa fa-eye"></i></div></button>'; //CREO ESTE BOTON 
+            $data[] = $sub_array;
+        }
+        //Para usar el DATATABLE
+        $results = array(
+            "sEcho" => 1,
+            "iTotalRecords" => count($data),
+            "iTotalDisplayRecords" => count($data),
+            "aaData" => $data
+        );
+        echo json_encode($results);
         break;
-            
-    
-    }
+
+        //PARA LISTAR EL DETALLE DE TICKET 
+    case "listardetalle":
+        $datos = $ticket->listar_ticketdetalle_x_ticket($_POST["tick_id"]);
+?>
+        <?php
+        foreach ($datos as $row) {
+        ?>
+            <article class="activity-line-item box-typical">
+                <div class="activity-line-date">
+                    <?php echo date("d/m/Y", strtotime($row["fech_crea"])); ?> <!-- aqui le coloco la fecha de creacion que tengo en mi tabla td_detalleticket-->
+                </div>
+                <header class="activity-line-item-header">
+                    <div class="activity-line-item-user">
+                        <div class="activity-line-item-user-photo">
+                            <a href="#">
+                                <img src="../../public/img/photo-64-2.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="activity-line-item-user-name"> <?php echo $row['usu_nom'] .' ' .$row['usu_ape'] ?></div>
+                        <!--AQUI PARA DECIDIR SI ES SOPORTE O USUARIO ESTANDAR Y LO MUESTRE EN MI detalleticket.php-->
+                        <div class="activity-line-item-user-status">
+                            <?php
+                                if($row['rol_id']==1){
+                                echo 'Usuario';
+                                }else{
+                                echo 'Soporte';
+                                }
+                                
+                             ?>
+                        </div>
+                    </div>
+                </header>
+                <div class="activity-line-action-list">
+                    <section class="activity-line-action">
+                        <div class="time"> <?php echo date("H:i:s", strtotime($row["fech_crea"])); ?></div>
+                        <div class="cont">
+                            <div class="cont-in">
+                                <p> <?php echo $row['tickd_descrip'] ?></p>
+                                
+                            </div>
+                        </div>
+                    </section><!--.activity-line-action-->
+                   
+                </div><!--.activity-line-action-list-->
+            </article><!--.activity-line-item-->
+        <?php
+        }
+        ?>
+<?php
+        break;
+}
 
 ?>
--- view/DetalleTicket/detalleTicket.js
+++ view/DetalleTicket/detalleTicket.js
@@ -5,15 +5,24 @@ function init(){
 
 
 
-$(document).ready(function(){
- var id= getUrlParameter('ID');
- console.log(id);
+$(document).ready(function() {
+    var tick_id = getUrlParameter('ID');
+    //console.log(id);
+    //AQUI LE PASO ESTO PARA QUE SELECCIONE MI listardetalle de mi ticket.php y le paso como parametro el id del ticket  
+     $.post("../../controller/ticket.php?op=listardetalle", {tick_id : tick_id}, function (data){
+        //console.log(data);   // esto es para ver que me lo imprima 5 veces
+        $('#lbldetalle').html(data);  //LLAMO  a mi section de mi index.php que conteinia a mi article
+     });
 });
+
+
+
 
 
 //FUNCION PARA CAPTURAR EL APRAMETRO DEL ID QUE SE PASA POR AL URL CUANDO SE  DA CLIC EN EL OJITO
 var getUrlParameter = function getUrlParameter(sParam){
-    var sPageURL= decodeURIComponent(window.location.search.substring(1));
+    //Declaracion de varialbes
+    var sPageURL= decodeURIComponent(window.location.search.substring(1)),
     sURLVariables= sPageURL.split('&'),
     sParameterName,
     i;
